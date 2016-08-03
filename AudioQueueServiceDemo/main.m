adfc4d616c48edd5bd185cff98c514a896e0bf02
brownfeng <brownfeng@webank.com>
2016-08-03 16:36:30 +0800

    Initial Commit

---
+++ AudioQueueServiceDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AudioQueueServiceDemo
+//
+//  Created by brownfeng on 16/8/3.
+//  Copyright © 2016年 brownfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
